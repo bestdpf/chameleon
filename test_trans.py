67c30823a15724a49595bb162768ca8bbb545b24
pengfeiduan <pengfeiduan@tencent.com>
2025-03-17 00:49:56 +0800

    add test

---
+++ test_trans.py
@@ -0,0 +1,18 @@
+from transformers import ChameleonForConditionalGeneration, ChameleonProcessor
+import torch
+from PIL import Image
+
+model_id = '../chameleon-7b'
+
+processor = ChameleonProcessor.from_pretrained(model_id)
+model = ChameleonForConditionalGeneration.from_pretrained(model_id, torch_dtype=torch.bfloat16)
+
+prompt = 'add a read hat for the photo <image>'
+img = Image.open('./test/liuyifei.png')
+
+inputs = processor(images=[img], text=prompt, return_tensors="pt").to(model.device, torch.bfloat16)
+
+generated_ids = model.generate(**inputs, max_new_tokens=1000, do_sample=False)
+ret = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+print(ret)
+
